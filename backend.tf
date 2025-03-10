e51ebced3e87dfa1cceb8ff7b82bea81a80d77f3
Abhinav Singh <abhinavsngh777@gmail.com>
2025-03-10 17:43:47 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-state-storage-847068433460"
     key            = "state/terraform-tk1.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
+    use_lockfile   = true        #dynamodb_table = "terraform-lock" is now deprecated
     profile        = "sandbox"
   }
 }
